a19ca43d157639ecc975599b1af145ce93ff9237
thesammet <thesammet@gmail.com>
2023-10-19 22:11:09 +0300

    addition: ios app tracking transparency

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './src/stack/home'
 import { SafeAreaView } from 'react-native';
 import color from './src/constants/color';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from './src/utils/toast';
+import { requestTrackingPermission } from 'react-native-tracking-transparency';
+const iosTrack = async () => {
+  const trackingStatus = await requestTrackingPermission();
+  if (trackingStatus === 'authorized' || trackingStatus === 'unavailable') {
+    //todo: enable tracking
+  }
+};
 function App() {
+  useEffect(() => {
+    Platform.OS === 'ios' && iosTrack();
+  }, []);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: color.bgBlue }}>
       <NavigationContainer>
